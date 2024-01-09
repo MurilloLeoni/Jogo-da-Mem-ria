c778de96e82b62967aff269ddfaee676bbbddbfb
murilloleoni <murilloleoni1999@gmail.com>
2024-01-09 13:43:44 -0300

    Efeitos para os cards

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -29,6 +29,20 @@ for(let i = 0; i < emojis.length; i++)
     let box = document.createElement("div");          //Criação dinamicamente de uma caixa "div"
     box.className = "item";                           //Associando "div" para a class = "item"
     box.innerHTML = randomCards[i];                   //Guarda os elementos da array na box aleatoriamente
+    box.onclick = handleClick;                        //Quando usuário clicar em algum card chama a função
     document.querySelector(".game").appendChild(box); //Pendurar box na div principal(game)
 }
+
+//Função para verificar, se o usário selecionou somente dois cards
+function handleClick(){
+    if(openCards.length < 2){
+        this.classList.add("boxOpen"); //Quando card for clicado é criado uma classe = "boxOpen"
+        openCards.push(this);
+    }
+    if(openCards.length == 2){         //Condição para checar se os cards são iguais
+        settimeout(checkMatch,500);
+    }
+}
+
+function checkMatch(){}
 
